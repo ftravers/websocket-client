cba05e70232b58840dd713cd4db0d2f244634653
Fenton Travers <fenton.travers@gmail.com>
2017-02-03 15:03:42 -0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to websocket-client
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/websocket_client/core.clj
@@ -0,0 +1,7 @@
+(ns websocket-client.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
