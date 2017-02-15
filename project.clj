d387ab5eb54f2b3e17b0b43e05e50cf69b50326c
Fenton Travers <fenton.travers@gmail.com>
2017-02-15 11:35:48 -0800

    Version 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fentontravers/websocket-client "0.4.0"
+(defproject fentontravers/websocket-client "0.4.1-SNAPSHOT"
   :description "WebSocket Client Library"
   :url "https://github.com/ftravers/websocket-client"
   :license {:name "Eclipse Public License"
